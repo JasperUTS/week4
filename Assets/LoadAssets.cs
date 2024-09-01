f1e7aad3aad65fe9167bbdacb40427cd8fe70759
Jay Overall <jayoverall@Jays-MacBook-Pro.local>
2024-09-01 17:53:19 +1000

    Instantiated prefabs and set consistent frame rate

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Instantiate the red object at position (2, 0, 0) with no rotation
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // Instantiate the blue object at position (-2, 0, 0) with no rotation
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
+        // Set the frame rate to 60 frames per second
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
