d0653a2562b98fccad2760f3ea7c0c418134d367
Jay Overall <jayoverall@Jays-MacBook-Pro.local>
2024-09-01 18:00:22 +1000

    Implemented ConsolePrint to print GameObject properties every frame

---
+++ Assets/ConsolePrint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3; // Initialize i to 3
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++; // Increment i by 1 at the start of every frame
+        Debug.Log(gameObject.name + ":" + i); // Print GameObject's name and the value of i
+
+    }
+}
